7a8bff600355568a7bb0cbb9768cc084ed637dc1
David zhang <d.ydzdavid@yahoo.com>
2019-03-06 16:54:45 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,11 +1,6 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 
-def meal_choice(value)
-    if value ==  "meat"
-      return value
-    else
-      return false 
-    end
+def meal_choice(value = "meat")
+    #{value}
   end
-  
